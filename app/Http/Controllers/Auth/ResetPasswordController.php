36d8249455fc209bd73d50fccdb9dfb841152325
chivunira <emmanuelchivunira@gmail.com>
2023-06-22 17:45:03 +0300

    pasword reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
@@ -26,5 +32,40 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function reset(Request $request)
+    {
+        $request->validate(
+            [
+                'token' => 'required',
+                'email' => 'required|email',
+                'password' => [
+                    'required',
+                    'confirmed',
+                    Password::min(8)
+                        ->letters()
+                        ->mixedCase()
+                        ->symbols()
+                ],
+            ]
+        );
+
+        // Find the user by email
+        $user = User::where('email', $request->email)->first();
+
+        // Error message when user does not exist
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => ['User does not exist!'],
+            ]);
+        }
+
+        // Update the user's password
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        // Redirect the user after password reset
+        return redirect('/user_login')->with('success', 'Password reset successfully');
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid border border-danger @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" onpaste="return false;" class="form-control @error('password') is-invalid border border-danger @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="error invalid-feedback" role="alert">
@@ -51,7 +51,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" onpaste="return false;" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
