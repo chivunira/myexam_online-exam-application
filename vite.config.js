4e97bc04c4261ac3e1d861fe9a418120cfaa5478
chivunira <emmanuelchivunira@gmail.com>
2023-07-07 15:09:39 +0300

    merge

--- vite.config.js
+++ vite.config.js
@@ -26,14 +26,3 @@ export default defineConfig({
         },
     },
 });
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
