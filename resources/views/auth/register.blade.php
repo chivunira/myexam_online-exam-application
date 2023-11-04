1985f76bf334a52d33b408fc5af52760ee765c87
chivunira <emmanuelchivunira@gmail.com>
2023-11-04 13:16:26 +0300

    registration changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,75 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <div class="row mb-3">
-                        @if(session('message'))
-                            <div class="alert alert-danger">
-                                {{ session('message') }}
-                            </div>
-                        @endif
-                    </div>
-                    
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid border border-danger @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="error invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" onpaste="return false;" class="form-control @error('password') is-invalid border border-danger @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="error invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" onpaste="return false;" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
-
 @extends('layouts.app')
 
 @section('title')
@@ -119,7 +47,6 @@
                                     @error('email')
                                         <div class="fw-light text-danger" >
                                             {{$message}}
-                    
                                         </div>
                                     @enderror
                                 </div>
@@ -137,13 +64,12 @@
                                 </div>
     
                                 <div class="form-group mb-3">
-                                    <label for="password-confirm">Password</label>
-                                    <input type="password" onpaste="return false;" class="form-control @error('password-confirm') border border-danger @enderror" id="password-confirm" name="password-confirm">
-    
-                                    @error('password-confirm')
-                                        <div class="fw-light text-danger" >
-                                            {{$message}}
-                    
+                                    <label for="password-confirmation">Password Confirmation</label>
+                                    <input type="password" class="form-control @error('password_confirmation') border border-danger @enderror" id="password-confirmation" name="password_confirmation">
+                                    
+                                    @error('password_confirmation')
+                                        <div class="fw-light text-danger">
+                                            {{ $message }}
                                         </div>
                                     @enderror
                                 </div>
